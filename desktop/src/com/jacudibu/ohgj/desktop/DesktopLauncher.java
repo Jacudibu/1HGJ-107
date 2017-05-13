78ea006b2d6e6a006f81f906c02e310528157dfa
Jacudibu <jacudibu@gmail.com>
2017-05-13 22:04:19 +0200

    Initial Commit.

---
+++ desktop/src/com/jacudibu/ohgj/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.jacudibu.ohgj.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.jacudibu.ohgj.Core;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Core(), config);
+	}
+}
---
+++ html/src/com/jacudibu/ohgj/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.jacudibu.ohgj.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.jacudibu.ohgj.Core;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener () {
+                return new Core();
+        }
+}
